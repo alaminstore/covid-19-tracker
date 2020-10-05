e368e310509ec4d0921521e87783e59c13ff30d2
Sm Al-amin <67338203+alaminstore@users.noreply.github.com>
2020-10-06 01:33:18 +0600

    Init stage of Map Creation

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import "./Map.css";
 
-function Map() {
+const Map = ({ countries, center, zoom }) => {
   return (
-    <div>
-      <h3>Map Here</h3>
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </LeafletMap>
     </div>
   );
-}
+};
 
 export default Map;
